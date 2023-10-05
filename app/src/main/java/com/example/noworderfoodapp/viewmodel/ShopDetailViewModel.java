30500a68666216519394ef128aa3145e2f7ff50f
nam09082001 <106221921+nam09082001@users.noreply.github.com>
2023-10-06 06:48:00 +0700

    now order food

---
+++ app/src/main/java/com/example/noworderfoodapp/viewmodel/ShopDetailViewModel.java
@@ -0,0 +1,20 @@
+package com.example.noworderfoodapp.viewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.noworderfoodapp.api.ApiService;
+import com.example.noworderfoodapp.entity.Orders;
+import com.example.noworderfoodapp.entity.ResponseDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ShopDetailViewModel extends ViewModel {
+
+}
